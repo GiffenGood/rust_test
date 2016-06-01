77ee37b0cadaa13d63a821899d4fce7a7aed416c
mbeckius@gmail.com <MBeckius@gmail.com>
2016-06-01 08:30:45 -0400

    more stuff

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,27 @@ use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
+    forTest();
+}
+
+fn forTest(){
+    for x in 1..10{
+        if (x % 2) == 0 { continue;}
+        println!("x {}",x);
+    }
+}
+
+fn testIf(val : i32){
+    let y = if val < 10 { "less than 10" } else {"gt 10"};
+    println!("the value is {}",y);
+}
+
+fn testTuple() {
+    let a = (1, 2, 3);
+    println!("test {}", a.1);
+}
+
+fn game() {
     println!("Guess the number!");
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
@@ -14,7 +35,8 @@ fn main() {
 
         let mut guess = String::new();
 
-        io::stdin().read_line(&mut guess)
+        io::stdin()
+            .read_line(&mut guess)
             .expect("failed to read line");
 
         let guess: u32 = match guess.trim().parse() {
@@ -25,9 +47,9 @@ fn main() {
         println!("You guessed: {}", guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less    => println!("Too small!"),
+            Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal   => {
+            Ordering::Equal => {
                 println!("You win!");
                 break;
             }
